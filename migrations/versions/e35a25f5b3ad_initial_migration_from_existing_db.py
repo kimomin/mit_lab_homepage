0977e717f7fe157109461350c4a21c52aa0b99e6
kimomin <ksiemomnign@gmail.com>
2025-09-05 14:34:24 +0900

    Fix(v1): DB migration issue

---
+++ migrations/versions/e35a25f5b3ad_initial_migration_from_existing_db.py
@@ -0,0 +1,24 @@
+"""Initial migration from existing DB
+
+Revision ID: e35a25f5b3ad
+Revises: 70d314d4e8ef
+Create Date: 2025-09-05 14:27:36.394286
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e35a25f5b3ad'
+down_revision = '70d314d4e8ef'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
